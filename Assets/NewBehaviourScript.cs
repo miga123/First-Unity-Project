8d0a42d6a3a967469480ebbf814e4d25252d5191
DESKTOP-PIVSOU3\deXter <migajaparidze555@gmail.com>
2017-04-12 20:50:40 +0400

    first try

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car: MonoBehaviour 
+{
+	public int numberofwheels = 6;
+	private	bool isstart = true;
+	public	float maxspeed = 256.6f;
+	
+	void StartThecar ();
+	{
+		isstart = true;
+}
+	void StopTheCar () {
+	public void SetMaxSpeed ();
+
+	}
