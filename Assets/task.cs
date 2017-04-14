fe574f6fb24aefd70feaf57b5edaee69119430e4
DESKTOP-PIVSOU3\deXter <migajaparidze555@gmail.com>
2017-04-14 19:47:08 +0400

    added Script

---
+++ Assets/task.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class task : MonoBehaviour {
+
+
+
+	int frameCounter = 0;
+
+	void Update () {
+		frameCounter = frameCounter + 1;
+		//Debug.Log(Time.time);
+
+		//if(0 == frameCounter % 2)
+			//Debug.Log (frameCounter);
+
+
+
+		float fps = frameCounter / Time.time;
+		Debug.Log(Time.time);
+
+		//if(frameCounter == 100)
+
+
+
+	}
+}
