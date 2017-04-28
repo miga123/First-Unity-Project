3c25a718e8548ffe82c90738ca192528c2f7e63d
DESKTOP-PIVSOU3\deXter <migajaparidze555@gmail.com>
2017-04-28 17:37:01 +0400

    new scripts

---
+++ Assets/BTUgameDev5.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTUgameDev5 : MonoBehaviour {
+
+   //public int a;
+   // public float b = 0.6326565f;
+   // public bool c = true;
+   // public string d = "this is next";
+   // public string nameChanger;
+
+      //public Vector3 position;
+    //public Vector3 speed;
+    //public Vector3 scale;
+   // public Vector3 scaleSpeed;
+    public Vector3 moveSpeed;
+    //public Vector3 rotSpeed;
+
+    public Transform othertransform;
+
+
+
+
+
+    void Start()
+    {
+        //  transform.name = "spere";
+
+
+        //change name
+        //  transform.name = nameChanger;
+
+
+        //   position = transform.position;
+
+
+
+    }
+    
+    void Update()
+    {
+
+        //position = position + speed;
+        /// transform.position = transform.position + moveSpeed;
+
+        // transform.localScale = transform.localScale + scale;
+
+
+        //transform.localScale = transform.localScale + scaleSpeed;
+
+        //transform.eulerAngles = transform.eulerAngles + rotSpeed;
+
+
+
+
+        //moving script
+        if (othertransform.position.x <= -200f)
+        {
+            moveSpeed = moveSpeed * -1f;
+        }
+        else if(othertransform.position.x >= 200f)
+        {
+            moveSpeed = moveSpeed * -1;
+        }
+
+        othertransform.position = othertransform.position + moveSpeed;
+    }
+
+
+}
--- Assets/task.cs
+++ Assets/task.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class task : MonoBehaviour {
 
+    int frameCounter = 0;
+    int frameDirection = 1;
+    void Update()
+    {
+        //frameCounter = frameCounter = 1;
 
+        // Debug.Log(Time.time);
 
-	int frameCounter = 0;
+        //if (0 == frameCounter % 2)
+        //  Debug.Log(frameCounter);
 
-	void Update () {
-		frameCounter = frameCounter + 1;
-		//Debug.Log(Time.time);
+        float fps = frameCounter / Time.time;
+        //Debug.Log(Time.time);
 
-		//if(0 == frameCounter % 2)
-			//Debug.Log (frameCounter);
+        if (frameCounter == 100)
+        {
+            //  frameCounter = (frameCounter - 100);
+            frameDirection = -1;
+        }
+        else if (frameCounter == 0)
+        {
+            frameCounter = 1;
+        }
+        frameCounter = frameCounter + frameDirection;
 
-
-
-		float fps = frameCounter / Time.time;
-		Debug.Log(Time.time);
-
-		//if(frameCounter == 100)
-
-
-
-	}
+        Debug.Log(frameCounter);
+    }
 }
