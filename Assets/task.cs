02c926e5746d6f813f94dffd8d84ef35f05278f3
DESKTOP-PIVSOU3\deXter <migajaparidze555@gmail.com>
2017-05-16 22:19:22 +0400

    jkfnvdfj

--- Assets/task.cs
+++ Assets/task.cs
@@ -3,32 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class task : MonoBehaviour {
-
     int frameCounter = 0;
     int frameDirection = 1;
     void Update()
     {
-        //frameCounter = frameCounter = 1;
+    //  frameCounter = frameCounter + 1;
 
-        // Debug.Log(Time.time);
+     // Debug.Log(Time.time);
+        
 
-        //if (0 == frameCounter % 2)
-        //  Debug.Log(frameCounter);
+    //  if (0 == frameCounter % 2)
+     // Debug.Log(frameCounter);
 
-        float fps = frameCounter / Time.time;
-        //Debug.Log(Time.time);
+     // float fps = frameCounter / Time.time;
+   //     Debug.Log(Time.time);
 
-        if (frameCounter == 100)
-        {
-            //  frameCounter = (frameCounter - 100);
-            frameDirection = -1;
-        }
-        else if (frameCounter == 0)
-        {
-            frameCounter = 1;
-        }
-        frameCounter = frameCounter + frameDirection;
+            if (frameCounter == 100)
+            {
+                frameCounter = (frameCounter - 100);
+                frameDirection = -1;
+            }
+            else if (frameCounter == 0)
+            {
+            frameCounter = (frameCounter - 0);
+                frameCounter = 1;
+            }
+            frameCounter = frameCounter + frameDirection;
 
-        Debug.Log(frameCounter);
+           Debug.Log(frameCounter);
+       
     }
 }
